2178640e09c234d7b3424dee10a700c31ce9b3f8
Kavindu Madushan <kavindu@tsdclanka.com>
2022-09-23 23:58:13 +0530

    IT20122614 and IT20122096 and IT20192082 and IT20216078

---
+++ src/main/java/com/SPM/backend/IT20216078/repository/TransportServicesRepository.java
@@ -0,0 +1,16 @@
+package com.SPM.repository;
+
+import com.SPM.domains.TransportServices;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransportServicesRepository extends MongoRepository<TransportServices, String> {
+    List<TransportServices> findTransportServicesByCompanyEmailAddressContainsIgnoreCaseOrCompanyNameContainsIgnoreCase(String companyName, String companyEmail);
+
+    TransportServices findTransportServicesByCompanyEmailAddress(String emailAddress);
+
+    void deleteByCompanyEmailAddress(String emailAddress);
+}
